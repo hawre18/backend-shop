d85421baa94733c69a65711248249670675e4267
shayan4122 <0930shayan4122@gmail.com>
2023-12-23 12:37:09 +0330

    shame on me

--- resources/views/index/admin/blogs/create.blade.php
+++ resources/views/index/admin/blogs/create.blade.php
@@ -7,7 +7,7 @@
             <nav aria-label="breadcrumb" class="d-inline-block mt-4 mt-sm-0">
                 <ul class="breadcrumb bg-transparent rounded mb-0 p-0">
                     <li class="breadcrumb-item"><a href="index.html">داکتریس</a></li>
-                    <li class="breadcrumb-item"><a href="doctors.html">پزشکان</a></li>
+                    <li class="breadcrumb-item"><a href="doctors.html">برنامه نویسان</a></li>
                     <li class="breadcrumb-item active" aria-current="page">افزودن پزشک</li>
                 </ul>
             </nav>
